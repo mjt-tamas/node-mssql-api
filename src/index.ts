23829276859ae763214afe327e9e6cd520a2dded
mjt-tamas <mjt.tamas@gmail.com>
2025-04-12 06:23:14 +0300

    added jest for testing

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
-import express from 'express';
-import routes from '~/routes';
-
-const app = express();
+import app from './app';
 const PORT = process.env.PORT || 3000;
-
-app.use(express.json());
-app.use('/api', routes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
